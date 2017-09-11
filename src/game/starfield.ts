6a6a0cbd653973d9970edd06fc72338d4b1d5d72
just nobody <kingdaro@gmail.com>
2017-09-11 16:23:59 -0400

    cleanup

--- src/game/starfield.ts
+++ src/game/starfield.ts
@@ -5,8 +5,8 @@ const WHITE = 0xffffff
 
 export class Starfield {
   container = new pixi.Container()
-  timer = 0
-  timerPeriod = 0.05
+  private timer = 0
+  private timerPeriod = 0.05
 
   constructor(private viewWidth: number, private viewHeight: number) {}
 
@@ -17,21 +17,28 @@ export class Starfield {
 
     if (this.timer >= this.timerPeriod) {
       this.timer -= this.timerPeriod
-
-      const star = new pixi.Graphics()
-      const size = randomBetween(2, 12)
-
-      star.beginFill(WHITE)
-      star.drawRect(0, 0, size, size)
-      star.endFill()
-
-      star.x = Math.random() * this.viewWidth
-      star.y = -100
-      star.pivot.set(5, 5)
-
-      this.container.addChild(star)
+      this.container.addChild(this.createStar())
     }
 
+    this.moveStars(dt)
+  }
+
+  private createStar() {
+    const star = new pixi.Graphics()
+    const size = randomBetween(2, 12)
+
+    star.beginFill(WHITE)
+    star.drawRect(0, 0, size, size)
+    star.endFill()
+
+    star.x = Math.random() * this.viewWidth
+    star.y = -100
+    star.pivot.set(5, 5)
+
+    return star
+  }
+
+  private moveStars(dt: number) {
     this.container.children.forEach((star: pixi.Graphics) => {
       star.y += star.height * 20 * dt
       if (star.y > this.viewHeight + star.height / 2) {
