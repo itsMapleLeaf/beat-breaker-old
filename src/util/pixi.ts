9e4dac214a6dd749d93e0340542e664b5a259aae
just nobody <kingdaro@gmail.com>
2017-09-12 09:50:41 -0400

    add an alpha argument to drawRect, + more utils

--- src/util/pixi.ts
+++ src/util/pixi.ts
@@ -1,8 +1,32 @@
 import * as pixi from 'pixi.js'
 
+type HasDimensions = {
+  width: number
+  height: number
+}
+
 /** Draw a rectangle to a pixi Graphics object with the given dimensions */
-export function drawRect(sprite: pixi.Graphics, width: number, height: number, color = 0xffffff) {
-  sprite.beginFill(color)
+export function drawRect(
+  sprite: pixi.Graphics,
+  width: number,
+  height: number,
+  color = 0xffffff,
+  alpha = 1,
+) {
+  sprite.beginFill(color, alpha)
   sprite.drawRect(0, 0, width, height)
   sprite.endFill()
+}
+
+/** Set an object's pivot to its center */
+export function pivotToCenter(obj: pixi.DisplayObject & HasDimensions) {
+  obj.pivot.set(obj.width / 2, obj.height / 2)
+}
+
+/** Create a centered rectangle object */
+export function createRect(width: number, height: number, color = 0xffffff, alpha = 1) {
+  const sprite = new pixi.Graphics()
+  drawRect(sprite, width, height, color, alpha)
+  pivotToCenter(sprite)
+  return sprite
 }
