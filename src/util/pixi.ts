deb64a0cd2991c8baab893543ea2c46879b88d3b
just nobody <kingdaro@gmail.com>
2017-09-12 09:23:14 -0400

    add optional color parameter to createRectObject

--- src/util/pixi.ts
+++ src/util/pixi.ts
@@ -1,9 +1,9 @@
 import * as pixi from 'pixi.js'
 
 /** Creates a rectangle pixi Graphics object with the given dimensions */
-export function createRectObject(width: number, height: number) {
+export function createRectObject(width: number, height: number, color = 0xffffff) {
   const sprite = new pixi.Graphics()
-  sprite.beginFill(0xffffff)
+  sprite.beginFill(color)
   sprite.drawRect(0, 0, width, height)
   sprite.endFill()
   return sprite
