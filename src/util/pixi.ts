9853c633dde887ff347a0fb344c8b29b378889d8
just nobody <kingdaro@gmail.com>
2017-09-12 09:24:21 -0400

    use a rectangle drawer instead so we can, for example, draw multiple rectangles to one graphics object

--- src/util/pixi.ts
+++ src/util/pixi.ts
@@ -1,10 +1,8 @@
 import * as pixi from 'pixi.js'
 
-/** Creates a rectangle pixi Graphics object with the given dimensions */
-export function createRectObject(width: number, height: number, color = 0xffffff) {
-  const sprite = new pixi.Graphics()
+/** Draw a rectangle to a pixi Graphics object with the given dimensions */
+export function drawRect(sprite: pixi.Graphics, width: number, height: number, color = 0xffffff) {
   sprite.beginFill(color)
   sprite.drawRect(0, 0, width, height)
   sprite.endFill()
-  return sprite
 }
