634c4db9d9323eb485c78bf7a057f167737f0cd4
just nobody <kingdaro@gmail.com>
2017-09-12 09:05:13 -0400

    add more colors for funsies

--- src/game/colors.ts
+++ src/game/colors.ts
@@ -1,4 +1,13 @@
 import * as Color from 'color'
 
-export const White = Color('white')
-export const Black = Color('black')
+// http://flatuicolors.com/
+export const Red = Color('rgb(231, 76, 60)')
+export const Orange = Color('rgb(230, 126, 34)')
+export const Yellow = Color('rgb(241, 196, 15)')
+export const Green = Color('rgb(46, 204, 113)')
+export const Turquoise = Color('rgb(26, 188, 156)')
+export const Blue = Color('rgb(52, 152, 219)')
+export const Purple = Color('rgb(155, 89, 182)')
+export const White = Color('rgb(236, 240, 241)')
+export const Black = Color('rgb(52, 73, 94)')
+export const Gray = Color('rgb(149, 165, 166)')
