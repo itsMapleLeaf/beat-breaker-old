347c1207480218031d7b6397f2619c9379f0847f
just nobody <kingdaro@gmail.com>
2017-09-12 08:22:08 -0400

    write webpack config in typescript

---
+++ webpack/webpack.config.ts
@@ -0,0 +1,43 @@
+import * as path from 'path'
+import * as webpack from 'webpack'
+import * as HTMLPlugin from 'html-webpack-plugin'
+
+const root = path.resolve(__dirname, '..')
+
+const config: webpack.Configuration = {
+  entry: path.resolve(root, 'src/main'),
+  output: {
+    path: path.resolve(root, 'build'),
+    filename: 'build.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  devServer: {
+    noInfo: true,
+  },
+  performance: {
+    hints: false,
+  },
+  plugins: [new HTMLPlugin({ template: path.resolve(root, 'src/index.html') })],
+  devtool: '#source-map',
+}
+
+if (process.env.NODE_ENV === 'production') {
+  // production
+  config.plugins.push(new webpack.optimize.ModuleConcatenationPlugin())
+} else {
+  // development
+  config.plugins.push(new webpack.NamedModulesPlugin())
+}
+
+export default config
